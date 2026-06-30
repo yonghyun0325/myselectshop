5578f980a384d3ba7101e1005f923a5fc8ea3f44
yonghyun0325 <dldydgus95426@gmail.com>
2026-07-01 01:59:36 +0900

    상품 폴더 설계

---
+++ src/main/java/com/example/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.example.myselectshop.repository;
+
+import com.example.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
